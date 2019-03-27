6af0139ef9bb5f89e7ad65e59ec982c318ade1b4
JD <jdfili14@gmail.com>
2019-03-26 20:27:08 -0700

    mostly working

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,8 @@ import React from "react"
 
 function Card(props) {
     return (
-        <div onClick = {() =>props.alertMe(props.id)} className="card text-center" style={{width: 18 +'rem'}}>
-            <img alt={props.name} src={props.image} class="card-img-top" />
+        <div onClick={()=>props.onClick(props.id)} className="card text-center" style={{width: 18 +'rem'}}>
+            <img alt={props.name} src={props.image} className="card-img-top" />
         </div>
     )
 
---
+++ src/components/Wrapper.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Wrapper(props) {
+  return <div className="ml-4 wrapper">{props.children}</div>;
+}
+
+export default Wrapper;
