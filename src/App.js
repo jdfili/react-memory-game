ff269cf1da13f7a3a7c00458d1002c355adc999f
JD <jdfili14@gmail.com>
2019-03-28 16:17:54 -0700

    stlyed a lil bit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import characters from "./friends.json"
 import Wrapper from "./components/Wrapper"
 import Jumbotron from './components/Jumbotron'
 import Card from './components/Card'
+import Nav from './components/Nav'
 
 class App extends Component {
 
@@ -15,10 +16,6 @@ class App extends Component {
     feedBack: ""
   }
 
-  //increments score +1
-  score = () => {
-    this.setState({ score: this.state.score + 1 });
-  };
   //compares the character id that was clicked, with the remaining character.id still in the characters array
   guessed = id => {
 
@@ -32,19 +29,19 @@ class App extends Component {
   //carries out proper steps depending on whether or not a match was found
   checkArr = (arr, id) => {
     if (arr.length === 1 && arr.includes(true)) {
-      this.setState({ feedBack: "You win!"});
-      this.setState({highScore: 10});
+      this.setState({ feedBack: "You win!" });
+      this.setState({ highScore: 10 });
       this.reset();
     }
     else if (arr.includes(true)) {
-      this.score();
+      this.setState({ score: this.state.score + 1 });
       const characters = this.state.characters.filter(character => character.id !== id);
       this.setState({ characters: characters });
       this.setState({ feedBack: "You've guessed correctly" })
       this.shuffle(this.state.baseCharacters)
     } else {
       if (this.state.score > this.state.highScore) {
-        this.setState({ highScore: this.state.score })
+        this.setState({ highScore: this.state.score });
       };
       this.setState({ feedBack: "You've guessed incorrectly" })
       this.reset();
@@ -57,27 +54,30 @@ class App extends Component {
     this.setState({ characters: this.state.baseCharacters })
   };
   //used to shuffle the baseCharacter array which displays the characters to the user
-  shuffle = (array) => {
+  shuffle = array => {
     array.sort(() => Math.random() - 0.5);
     this.setState({ baseCharacters: array })
   };
   render() {
     return (
-      <Wrapper>
-        <h3>Score: {this.state.score}</h3>
-        <h3>High Score: {this.state.highScore}</h3>
-        <h3>{this.state.feedBack ? this.state.feedBack: "Click a character to begin"}</h3>
-        <Jumbotron>
-          {this.state.baseCharacters.map(character => {
-            return <Card
-              onClick={this.guessed}
-              key={character.id}
-              id={character.id}
-              alt={character.name}
-              image={character.image} />
-          })}
-        </Jumbotron>
-      </Wrapper>
+      <div>
+        <Nav
+          score={this.state.score}
+          highScore={this.state.highScore}
+          feedback={this.state.feedBack ? this.state.feedBack : "Click a character to begin"} />
+        <Wrapper>
+          <Jumbotron>
+            {this.state.baseCharacters.map(character => {
+              return <Card
+                onClick={this.guessed}
+                key={character.id}
+                id={character.id}
+                alt={character.name}
+                image={character.image} />
+            })}
+          </Jumbotron>
+        </Wrapper>
+      </div>
     )
   }
 }
